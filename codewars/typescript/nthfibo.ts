ddd6f7ae4eec29e5d776220d0b75bbc4df4f720e
mylo <milopadma@yahoo.com>
2023-02-01 19:14:26 +0800

    misunderstood the q

--- codewars/typescript/nthfibo.ts
+++ codewars/typescript/nthfibo.ts
@@ -1,31 +1,25 @@
+// function that retunrs the fibonacci number at the nth position
 export function nthFibo(n: number): number {
-  let nth = 0;
+  let nth = 1;
 
   // the fibonacci sequence is
   // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, etc...
-  // to find the nth fibonacci number
   let lastTwo = [0, 1];
 
   while (true) {
-    // the current fibonacci number is the sum of the last two numbers
-    let currentFibo = lastTwo[0] + lastTwo[1];
-
-    // check if the current number is a valid fibonacci number
-    if (currentFibo === n) {
-      console.log("currentFibo: " + currentFibo);
-      return nth;
+    if (nth === n) {
+      return lastTwo[0];
     } else {
-      // increase our current nth number
-      nth += 1;
+      nth++;
 
-      // update the last two integers for the fibonacci sequence
       lastTwo = [lastTwo[1], lastTwo[0] + lastTwo[1]];
     }
   }
 }
 
-console.log(nthFibo(1));
-console.log(nthFibo(2));
-console.log(nthFibo(3));
-console.log(nthFibo(4));
-console.log(nthFibo(7));
+// console log tests
+console.log(nthFibo(1)); // 0
+console.log(nthFibo(2)); // 1
+console.log(nthFibo(3)); // 1
+console.log(nthFibo(4)); // 2
+console.log(nthFibo(5)); // 3

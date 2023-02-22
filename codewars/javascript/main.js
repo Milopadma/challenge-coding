3599747d611183415d8eca7fe5819880e8a976c1
mylo <milopadma@yahoo.com>
2023-02-22 12:35:35 +0800

    fix

--- codewars/javascript/main.js
+++ codewars/javascript/main.js
@@ -8,8 +8,20 @@ function sumOfMinimum(nestedArray) {
     for (let j = 0; j < nestedArray[i].length; j++) {
       // find the minimum value in each array
       min.push(Math.min(...nestedArray[i]));
+      console.log(min);
     }
   }
+  //remove duplicates
+  min = [...new Set(min)];
+
   //return the sum of the minimum values
   return min.reduce((a, b) => a + b, 0);
 }
+
+console.log(
+  sumOfMinimum([
+    [1, 2, 3, 4, 5],
+    [5, 6, 7, 8, 9],
+    [20, 21, 34, 56, 100],
+  ])
+);

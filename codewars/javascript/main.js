2d796b71a201a2cc02104e55d49833ad6b9bbbaf
mylo <milopadma@yahoo.com>
2023-02-23 17:04:32 +0800

    help

--- codewars/javascript/main.js
+++ codewars/javascript/main.js
@@ -20,3 +20,23 @@ console.log(
     [20, 21, 34, 56, 100],
   ])
 );
+
+// function to return the count of the number of each letter in a string
+const orderedCount = function (text) {
+  // store the count of each letter in an object outside the loop
+  let letterCount = {};
+  // loop through the string
+  for (let i = 0; i < text.length; i++) {
+    // if the letter is not in the object, add it with a count of 1
+    if (!letterCount[text[i]]) {
+      letterCount[text[i]] = 1;
+    } else {
+      // if the letter is in the object, increment the count by 1
+      letterCount[text[i]]++;
+    }
+  }
+  // return the object as an array of arrays
+  return Object.entries(letterCount);
+};
+
+console.log(orderedCount("abracadabra"));

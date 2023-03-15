11bf6dc0b651d8774e6b1deb7aa6ccc1d1c5f013
mylo <48256865+Milopadma@users.noreply.github.com>
2023-03-15 10:00:58 +0800

    off by one moment

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -429,11 +429,11 @@ mod insertion_sort_list_leetcode {
         pub fn two_sum(mut nums: Vec<i32>, target: i32) -> Vec<i32> {
             let mut result = vec![];
             for i in 0..nums.len() - 1 {
-                if nums[i] + nums[i + 1] == target {
-                    nums.remove(i);
-                    result.push(i as i32);
-                } else {
-                    continue;
+                for j in i + 1..nums.len() {
+                    if nums[i] + nums[j] == target {
+                        result.push(i as i32);
+                        result.push(j as i32);
+                    }
                 }
             }
             result

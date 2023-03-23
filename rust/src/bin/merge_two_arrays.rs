aaf10b7fa5ef23e2fe6f94c58df5f397c8362c81
mylo <48256865+Milopadma@users.noreply.github.com>
2023-03-23 18:43:52 +0800

    wow that was not expected, simple

--- rust/src/bin/merge_two_arrays.rs
+++ rust/src/bin/merge_two_arrays.rs
@@ -2,18 +2,12 @@ mod merge_sorted_array_lc {
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
         // trim out the 0s
         nums1.truncate(m as usize); // uses m (which is the length of actual numbers before 0) to remove the 0s
-                                    // iterate and evaluate, O of n^2
-        for (i, num) in nums1.clone().iter().enumerate() {
-            for (i2, num2) in nums2.iter().enumerate() {
-                if *num2 as i32 >= *num as i32 {
-                    nums1.insert(i, *num2); // focus here
-                    nums2.remove(i2);
-                    break;
-                } else {
-                    continue;
-                }
-            }
+
+        for (i, num) in nums2.iter().enumerate() {
+            nums1.push(*num); // push the nums2 into nums1
         }
+
+        nums1.sort(); // sort the nums1
     }
 }
 

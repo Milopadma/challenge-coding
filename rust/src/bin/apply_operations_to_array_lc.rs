0e1ff790ee81a4cbb76d6d35b4e203d3a2a7a86e
mylo <48256865+Milopadma@users.noreply.github.com>
2023-03-25 21:52:45 +0800

    done

--- rust/src/bin/apply_operations_to_array_lc.rs
+++ rust/src/bin/apply_operations_to_array_lc.rs
@@ -1,2 +1,20 @@
-pub fn apply_operations(nums: Vec<i32>) -> Vec<i32> {}
-pub fn main() {}
+pub fn apply_operations(mut nums: Vec<i32>) -> Vec<i32> {
+    for i in 0..nums.len() - 1 {
+        if nums[i] == nums[i + 1] {
+            nums[i] *= 2;
+            nums[i + 1] = 0;
+        } else {
+            continue;
+        }
+    }
+    // to shift all 0s to the end of the array
+    nums.iter()
+        .filter(|&x| *x != 0) // takes the 0s out
+        .chain(nums.iter().filter(|&x| *x == 0)) // puts them at the back
+        .cloned()
+        .collect()
+}
+pub fn main() {
+    let nums = vec![1, 0, 0, 2, 3, 4, 5, 6, 7, 8, 9];
+    println!("{:?}", apply_operations(nums));
+}

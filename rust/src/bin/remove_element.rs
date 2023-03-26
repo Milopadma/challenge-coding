1380afa1743b7d0f6a1b9c5970d5f8232018a0b7
mylo <48256865+Milopadma@users.noreply.github.com>
2023-03-26 22:45:34 +0800

    wrestling

--- rust/src/bin/remove_element.rs
+++ rust/src/bin/remove_element.rs
@@ -1,9 +1,15 @@
 pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-    for (i, nums) in nums.iter().enumerate() {
-        if *nums == val {
-            nums.remove(i);
+    for (i, num) in nums.clone().iter().enumerate() {
+        if *num == val {
+            nums.swap(i, nums.len() - 1);
         }
     }
+    nums.len() as i32
 }
 
-pub fn main() {}
+pub fn main() {
+    let mut nums = vec![3, 2, 2, 3];
+    let val = 3;
+    let len = remove_element(&mut nums, val);
+    println!("len: {}, nums: {:?}", len, nums);
+}

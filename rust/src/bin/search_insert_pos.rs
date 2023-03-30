cc7170c89c615881b1d0450b947681927f7438a2
mylo <48256865+Milopadma@users.noreply.github.com>
2023-03-30 15:19:50 +0800

    usize non negative fix

--- rust/src/bin/search_insert_pos.rs
+++ rust/src/bin/search_insert_pos.rs
@@ -1,19 +1,28 @@
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    let mut left = 0;
-    let mut right = nums.len() - 1;
+    let mut left: usize = 0;
+    let mut right: usize = nums.len() - 1;
+
     while left <= right {
-        let mid = (left + right) / 2;
+        let mid: usize = left + (right - left) / 2;
+
         if nums[mid] == target {
             return mid as i32;
         } else if nums[mid] < target {
             left = mid + 1;
         } else {
+            if mid == 0 {
+                break;
+            }
             right = mid - 1;
         }
     }
+
+    // At this point, the target is not in the array, so we return the
+    // index where it should be inserted.
     left as i32
 }
 pub fn main() {
+    // generate test cases for search_insert
     let nums = vec![1, 3, 5, 6];
     let target = 5;
     let result = search_insert(nums, target);

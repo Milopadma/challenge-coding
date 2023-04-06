cab3726738101bf727254c45a2ba463da08f2d6d
mylo <milopadma@yahoo.com>
2023-04-06 18:44:33 +0800

    fixed

--- rust/src/bin/number_of_closed_islands.rs
+++ rust/src/bin/number_of_closed_islands.rs
@@ -1,28 +1,30 @@
-pub fn dfs(grid: &mut Vec<Vec<i32>>, i: usize, j: usize) -> bool {
-    if i == 0 || j == 0 || i == grid.len() - 1 || j == grid[0].len() - 1 {
-        return false;
-    }
-    if grid[i][j] == 1 {
+// depth first search algorithm to find out
+// if the island is closed or not by checking
+// if its at the edge, and
+// if any of its 4 sides are 1s (up, down, left, right)
+fn dfs(grid: &mut Vec<Vec<i32>>, i: usize, j: usize) -> bool {
+    if i < 0 || i >= grid.len() || j < 0 || j >= grid[0].len() || grid[i][j] == 1 {
         return true;
     }
+    if (i == 0 || i == grid.len() - 1 || j == 0 || j == grid[0].len() - 1) && grid[i][j] == 0 {
+        return false;
+    }
+
     grid[i][j] = 1;
-    let mut res = true;
-    res &= dfs(grid, i + 1, j);
-    res &= dfs(grid, i - 1, j);
-    res &= dfs(grid, i, j + 1);
-    res &= dfs(grid, i, j - 1);
-    res
+
+    let top = dfs(grid, i - 1, j);
+    let bottom = dfs(grid, i + 1, j);
+    let left = dfs(grid, i, j - 1);
+    let right = dfs(grid, i, j + 1);
+
+    top && bottom && left && right
 }
 
-pub fn closed_islands(grid: Vec<Vec<i32>>) -> i32 {
-    // 0: land
-    // 1: water
-    // find a closed island, where all 4 sides of 0 are 1s
-    // return the number of closed islands
+pub fn closed_island(grid: Vec<Vec<i32>>) -> i32 {
     let mut grid = grid;
     let mut count = 0;
-    for i in 0..grid.len() {
-        for j in 0..grid[0].len() {
+    for i in 1..grid.len() - 1 {
+        for j in 1..grid[0].len() - 1 {
             if grid[i][j] == 0 {
                 if dfs(&mut grid, i, j) {
                     count += 1;
@@ -42,5 +44,5 @@ pub fn main() {
         vec![1, 0, 0, 1, 0, 1, 0, 1],
         vec![1, 1, 1, 1, 1, 1, 1, 0],
     ];
-    println!("{:?}", closed_islands(grid));
+    println!("{:?}", closed_island(grid));
 }

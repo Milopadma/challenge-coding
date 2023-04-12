e722bdd615bc1b7d8bcfdf3986bff365d32e9dab
mylo <48256865+Milopadma@users.noreply.github.com>
2023-04-12 14:47:48 +0800

    changed to non negative type

--- rust/src/bin/sqrt.rs
+++ rust/src/bin/sqrt.rs
@@ -1,9 +1,9 @@
 pub fn my_sqrt(x: i32) -> i32 {
-    let mut i = 0;
-    while i * i <= x {
+    let mut i: usize = 0;
+    while i * i <= x as usize {
         i += 1;
     }
-    i - 1
+    (i - 1) as i32
 }
 pub fn main() {
     println!("{}", my_sqrt(2));

66cc889572c12e64174d309f6165561acc0fb2c8
mylo <milopadma@yahoo.com>
2023-04-15 14:40:31 +0800

    climbing stairs

---
+++ rust/src/bin/climbing_stairs.rs
@@ -0,0 +1,12 @@
+pub fn climb_stairs(n: i32) -> i32 {
+    let mut dp = vec![0; n as usize + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+    for i in 2..=n as usize {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    dp[n as usize]
+}
+pub fn main() {
+    println!("{}", climb_stairs(3));
+}

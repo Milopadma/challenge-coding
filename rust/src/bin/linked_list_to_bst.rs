6f2889d40467a3482407779b02ee132f375df2ef
mylo <48256865+Milopadma@users.noreply.github.com>
2023-04-21 14:44:33 +0800

    linked list to bst

---
+++ rust/src/bin/linked_list_to_bst.rs
@@ -0,0 +1,22 @@
+use std::{cell::RefCell, rc::Rc};
+
+pub fn sorted_list_to_bst(head: Option<Box<ListNode>>) -> Option<Rc<RefCell<TreeNode>>> {
+    let mut nums = Vec::new();
+    let mut head = head;
+    while let Some(node) = head {
+        nums.push(node.val);
+        head = node.next;
+    }
+    build_tree(&nums)
+}
+
+fn build_tree(nums: &Vec<i32>) -> Option<Rc<RefCell<_>>> {
+    if nums.is_empty() {
+        return None;
+    }
+    let mid = nums.len() / 2;
+    let mut root = TreeNode::new(nums[mid]);
+    root.left = build_tree(&nums[..mid]);
+    root.right = build_tree(&nums[mid + 1..]);
+    Some(Rc::new(RefCell::new(root)))
+}

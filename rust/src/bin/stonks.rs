23d55b0a411878466538f99092a4f19662267a84
mylo <48256865+Milopadma@users.noreply.github.com>
2023-04-29 14:04:16 +0800

    📝 docs(stonks.rs): add comments to improve code readability and explain the purpose of variables and loops

--- rust/src/bin/stonks.rs
+++ rust/src/bin/stonks.rs
@@ -2,7 +2,11 @@
 // finds out the max differential increase from day i to day j
 pub fn max_profit(prices: Vec<i32>) -> i32 {
     let mut max_profit = 0;
+    // keep track of the min price seen so far because we want to buy at the
+    // lowest price
     let mut min_price = prices[0];
+    // iterate through the prices and find the max profit
+    // by differential of the min price seen so far
     for i in 1..prices.len() {
         if prices[i] < min_price {
             min_price = prices[i];

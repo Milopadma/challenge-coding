7f35ad8f927f364591b100683edcdfda7f6ce274
mylo <48256865+Milopadma@users.noreply.github.com>
2023-04-29 13:57:25 +0800

    🚀 feat(stonks.rs): add max_profit function to calculate the maximum profit from a vector of prices 🎨 style(stonks.rs): format code to improve readability and add comments to the code

--- rust/src/bin/stonks.rs
+++ rust/src/bin/stonks.rs
@@ -1,2 +1,19 @@
-pub fn max_profit(prices: Vec<i32>) -> i32 {}
-pub fn main() {}
+// max profit function given a vector of prices at i-index of days,
+// finds out the max differential increase from day i to day j
+pub fn max_profit(prices: Vec<i32>) -> i32 {
+    let mut max_profit = 0;
+    let mut min_price = prices[0];
+    for i in 1..prices.len() {
+        if prices[i] < min_price {
+            min_price = prices[i];
+        } else if prices[i] - min_price > max_profit {
+            max_profit = prices[i] - min_price;
+        }
+    }
+    max_profit
+}
+pub fn main() {
+    let prices = vec![7, 1, 5, 3, 6, 4];
+    let max_profit = max_profit(prices);
+    println!("Max profit is {}", max_profit);
+}

901345dbb5e35a254824343f4184a0373c97d9a7
mylo <milopadma@yahoo.com>
2023-05-09 14:36:05 +0800

    🐛 fix(spiral_matrix.rs): add edge cases for single row, single column and 2x2 matrix ✨ feat(spiral_matrix.rs): add explicit type annotations for row_start, row_end, col_start and col_end variables

--- rust/src/bin/spiral_matrix.rs
+++ rust/src/bin/spiral_matrix.rs
@@ -2,12 +2,28 @@
 // and returns an array with
 // the elements in spiral order
 pub fn spiral_order(matrix: Vec<Vec<i32>>) -> Vec<i32> {
+    // check of edge cases [[n]] || [[n,n]] || [[n], [n], [n]] cases
+    if matrix.len() == 1 {
+        return matrix[0].clone();
+    } else if matrix.len() == 2 {
+        let mut result: Vec<i32> = Vec::new();
+        result.extend(matrix[0].clone());
+        result.extend(matrix[1].clone().into_iter().rev());
+        return result;
+    } else if matrix[0].len() == 1 {
+        let mut result: Vec<i32> = Vec::new();
+        for i in 0..matrix.len() {
+            result.push(matrix[i][0]);
+        }
+        return result;
+    }
+
     let mut result: Vec<i32> = Vec::new();
     // by keeping track on the starts and ends of the rows and columns
-    let mut row_start = 0;
-    let mut row_end = matrix.len() - 1;
-    let mut col_start = 0;
-    let mut col_end = matrix[0].len() - 1;
+    let mut row_start: usize = 0;
+    let mut row_end: usize = matrix.len() - 1;
+    let mut col_start: usize = 0;
+    let mut col_end: usize = matrix[0].len() - 1;
 
     // if the row start index is greater than the row end index
     // and the column start index is greater than the column end index

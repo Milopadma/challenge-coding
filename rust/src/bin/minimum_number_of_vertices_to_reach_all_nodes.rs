f19e7369e0ee799cb10f164b2f012a792bd303ee
mylo <milopadma@yahoo.com>
2023-05-18 16:48:00 +0800

    ✨ feat(minimum_number_of_vertices_to_reach_all_nodes.rs): add function to find smallest set of vertices to reach all nodes

---
+++ rust/src/bin/minimum_number_of_vertices_to_reach_all_nodes.rs
@@ -0,0 +1,22 @@
+pub fn find_smallest_set_of_verteces(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
+    let mut res = vec![];
+    // the indegree is the number of edges pointing to a node
+    let mut indegree = vec![0; n as usize];
+    // iterate through every node edges and count the indegrees for that node
+    for edge in edges {
+        indegree[edge[1] as usize] += 1;
+    }
+    // if the indegree is 0, then it means that there is no edge pointing to that node
+    for i in 0..n {
+        if indegree[i as usize] == 0 {
+            res.push(i);
+        }
+    }
+    res
+}
+pub fn main() {
+    let n = 6;
+    let edges = vec![vec![0, 1], vec![0, 2], vec![2, 5], vec![3, 4], vec![4, 2]];
+    let res = find_smallest_set_of_verteces(n, edges);
+    println!("{:?}", res);
+}

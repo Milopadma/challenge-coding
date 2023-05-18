0a83f7a34e3f4b36c3320186c6510fe425c82443
mylo <milopadma@yahoo.com>
2023-05-18 16:50:34 +0800

    🐎 perf(minimum_number_of_vertices_to_reach_all_nodes.rs): change for loop to filter and map for 16% speed increase

--- rust/src/bin/minimum_number_of_vertices_to_reach_all_nodes.rs
+++ rust/src/bin/minimum_number_of_vertices_to_reach_all_nodes.rs
@@ -1,5 +1,4 @@
 pub fn find_smallest_set_of_verteces(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
-    let mut res = vec![];
     // the indegree is the number of edges pointing to a node
     let mut indegree = vec![0; n as usize];
     // iterate through every node edges and count the indegrees for that node
@@ -7,12 +6,13 @@ pub fn find_smallest_set_of_verteces(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
         indegree[edge[1] as usize] += 1;
     }
     // if the indegree is 0, then it means that there is no edge pointing to that node
-    for i in 0..n {
-        if indegree[i as usize] == 0 {
-            res.push(i);
-        }
-    }
-    res
+    // change from for loop to a filter and map for a 16% speed increase
+    indegree
+        .iter()
+        .enumerate()
+        .filter(|(_, &x)| x == 0)
+        .map(|(i, _)| i as i32)
+        .collect()
 }
 pub fn main() {
     let n = 6;

bb54ab627cc7ba599ec408b26549079fd30f4e9a
mylo <48256865+Milopadma@users.noreply.github.com>
2023-06-01 23:51:06 +0800

    🔨 refactor(underground_system.rs): change transactions to check_ins and travel_times to improve semantics ✨ feat(underground_system.rs): add check_ins and travel_times hashmaps to store check-in and travel time data respectively 🔨 refactor(underground_system.rs): change check_in and check_out methods to use check_ins and travel_times hashmaps instead of transactions to improve semantics ✨ feat(underground_system.rs): add get_average_time method to calculate average travel time between two stations based on check-in and check-out data

--- rust/src/bin/underground_system.rs
+++ rust/src/bin/underground_system.rs
@@ -1,67 +1,37 @@
 use std::collections::HashMap;
-#[derive(Clone)]
-struct Transaction {
-    start_station_name: String,
-    end_station_name: String,
-    start_time: i32,
-    end_time: i32,
+
+pub struct UndergroundSystem {
+    check_ins: HashMap<i32, (String, i32)>,
+    travel_times: HashMap<(String, String), (i32, i32)>,
 }
 
-struct UndergroundSystem {
-    // a hash set of ID and transaction
-    transactions: HashMap<i32, Transaction>,
-}
-
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl UndergroundSystem {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
-            transactions: HashMap::new(),
+            check_ins: HashMap::new(),
+            travel_times: HashMap::new(),
         }
     }
 
-    fn check_in(&mut self, id: i32, station_name: String, t: i32) {
-        self.transactions.insert(
-            id,
-            Transaction {
-                start_station_name: station_name,
-                end_station_name: "".to_string(),
-                start_time: t,
-                end_time: 0,
-            },
-        );
+    pub fn check_in(&mut self, id: i32, station_name: String, t: i32) {
+        self.check_ins.insert(id, (station_name, t));
     }
 
-    fn check_out(&self, id: i32, station_name: String, t: i32) {
-        // check if the id is in the transactions
-        if let Some(transaction) = self.transactions.get(&id) {
-            // update the end station name and end time
-            let mut transaction = transaction.clone();
-            transaction.end_station_name = station_name;
-            transaction.end_time = t;
-            // update the transaction
-            self.transactions.insert(id, transaction.clone());
+    pub fn check_out(&mut self, id: i32, station_name: String, t: i32) {
+        if let Some((start_station, start_time)) = self.check_ins.remove(&id) {
+            let key = (start_station, station_name);
+            let (total_time, count) = self.travel_times.get(&key).unwrap_or(&(0, 0)).clone();
+            self.travel_times
+                .insert(key, (total_time + (t - start_time), count + 1));
         }
     }
 
-    fn get_average_time(&self, start_station: String, end_station: String) -> f64 {
-        // takes the average time it takes to travel from start_station to end_station.
-        // The average time is computed from all the previous traveling from start_station to end_station that happened directly.
-        // Call to getAverageTime is always valid.
-        let mut total_time = 0;
-        let mut total_count = 0;
-        for (_, transaction) in self.transactions.iter() {
-            if transaction.start_station_name == start_station
-                && transaction.end_station_name == end_station
-            {
-                total_time += transaction.end_time - transaction.start_time;
-                total_count += 1;
-            }
+    pub fn get_average_time(&self, start_station: String, end_station: String) -> f64 {
+        if let Some((total_time, count)) = self.travel_times.get(&(start_station, end_station)) {
+            *total_time as f64 / *count as f64
+        } else {
+            0.0
         }
-        total_time as f64 / total_count as f64
     }
 }
 

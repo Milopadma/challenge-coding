5149a0935d65294e79affad35916551c30341bef
mylo <milopadma@yahoo.com>
2023-09-30 21:06:30 +0800

    comments

--- rust/src/bin/132_pattern.rs
+++ rust/src/bin/132_pattern.rs
@@ -2,11 +2,11 @@ struct Solution;
 
 impl Solution {
     pub fn find132pattern(nums: Vec<i32>) -> bool {
-        let mut stack = vec![];
-        let mut s3 = std::i32::MIN;
+        let mut stack = vec![]; // keep track of possible 2nd numbers in a stack
+        let mut s3 = std::i32::MIN; // keep track of the 3rd number
         for i in (0..nums.len()).rev() {
             if nums[i] < s3 {
-                return true;
+                return true; // if we find a 1st number that is smaller than the 3rd number, we found a 132 pattern
             }
             while !stack.is_empty() && nums[i] > stack[stack.len() - 1] {
                 s3 = stack.pop().unwrap();
